126a4c53092ee61258f66371afe7adf2140d4a01
Altaf Sayeed <altafsayeed19@yahoo.com>
2021-02-17 18:28:02 -0800

    deploy to gh pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
+
+import NewsCards from "./components/NewsCards/NewsCards";
 
 const alanKey =
   "056fc4b07f72ea122fb680ae426c27ad2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
       onCommand: ({ command, articles }) => {
         if (command === "newHeadlines") {
-          console.log(articles);
+          setNewsArticles(articles);
         }
       },
     });
@@ -19,6 +23,7 @@ const App = () => {
   return (
     <div>
       <h1>Alan AI News</h1>
+      <NewsCards articles={articles} />
     </div>
   );
 };
