70cfc2744b560103d97b1bf757f02936bf0468c5
Altaf Sayeed <altafsayeed19@yahoo.com>
2021-02-17 19:09:29 -0800

    deploy to gh pages

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,7 +1,54 @@
 import React from "react";
+import {
+  Card,
+  CardActions,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
 
-const NewsCard = () => {
-  return <div>News Card</div>;
+const NewsCard = ({
+  article: { description, publishedAt, source, title, url, urlToImage },
+  i,
+}) => {
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia
+          image={
+            urlToImage ||
+            "https://www.electrocore.com/wp-content/uploads/2017/12/article_generic_1440x634px-e1515567133169no-news.jpg"
+          }
+        />
+        <div>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {new Date(publishedAt).toDateString()}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {source.name}
+          </Typography>
+        </div>
+        <Typography gutterBottom variant="h5">
+          {title}
+        </Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+        <Typography variant="h5" color="textSecondary">
+          {i + 1}
+        </Typography>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default NewsCard;
