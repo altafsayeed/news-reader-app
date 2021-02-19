34d2905304d2a5967c622413cf6a28060f6a34ac
Altaf Sayeed <altafsayeed19@yahoo.com>
2021-02-19 00:25:47 -0800

    deploy to gh pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
+import wordsToNumbers from "words-to-numbers";
+
 import NewsCards from "./components/NewsCards/NewsCards";
 import useStyles from "./styles.js";
 
@@ -15,12 +17,27 @@ const App = () => {
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, number }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles);
           setActiveArticle(-1);
         } else if (command === "highlight") {
           setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn().playText(
+              "Silly goose, there are only 20 articles displayed. Please try again."
+            );
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          }
         }
       },
     });
